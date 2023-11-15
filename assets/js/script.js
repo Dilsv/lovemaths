9fd4388361c1c7d35c3babf3b75fbda310e37df1
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2023-11-15 22:16:32 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAsnwer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion () {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
