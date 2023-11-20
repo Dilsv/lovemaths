0534f00b2971c8b6309ba86f40376d93fa1dddd1
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2023-11-20 17:34:42 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,6 +76,9 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+        else if (operator === "-") {
+            return [operand1 - operand2,"subtract"];
+        }
     } else {
         alert ('Unimplemented operator $(oerator)');
         throw 'Unimplemented operator $(operator). Aborting!';
@@ -106,8 +109,10 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator'). textContent = "+";
 }
 
-function displaySubtractQuestion () {
-
+function displaySubtractQuestion (operand1, operand2) {
+    document.getElementById("operand1") = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2") = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator'). textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
